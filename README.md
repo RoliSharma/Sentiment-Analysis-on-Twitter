52f01d916e17f058611ae3c8635efc2b898a3de6
Roli Sharma <rolisharma.3900@gmail.com>
2021-03-01 23:43:00 +0530

    extract text part from tweets

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Sentiment-Analysis-on-Twitter
--Retrieving tweets using Tweepy API -Data preprocessing -Feature Extraction -Training Dataset -Naive Bayes Classification -Output
+-Retrieving tweets using Tweepy API 
+-Data preprocessing -Feature Extraction 
+-Training Dataset 
+-Naive Bayes Classification 
+-Output
+
+
+# Tweepy API documentation
+
+https://docs.tweepy.org/en/v3.10.0/api.html#API.search
+API.search(q[, geocode][, lang][, locale][, result_type][, count][, until][, since_id][, max_id][, include_entities])
--- TweepyAPI.py
+++ TweepyAPI.py
@@ -1,4 +1,5 @@
 import tweepy
+import json
 
 access_token="1306511356775010305-RW0inFN6dMQgYsaCNel5CcNtgO7aRT"
 access_token_secret="R6KXPDe1qOjNj8yt0sup0jh80eTKkODi9hGvnIlSN9kWx"
@@ -10,6 +11,22 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+
+
+#tweepy.search_30_day(dataset, query[, tag][, fromDate][, toDate][, maxResults][, next]);
+
+search_results = api.search(q="Banasthali", count=3)
+
+#lists = [[] for i in search_results]   
+
+dataset = [] 
+
+for i in search_results:
+    dataset.append(i._json)
+    #print(i)
+    #dataset.append(json.dumps(i._json, indent=4))
+    
+for i in dataset:
+    #print(i)
+    print(i["text"])
+    print("--------------------")
