11cf67a7f2bbf5ac212098a96d6713dd9cfd4e5f
courageoustouch <rashiguptafeb@gmail.com>
2021-04-25 09:54:04 +0530

    commiting training file

--- training.py
+++ training.py
@@ -1,36 +1,47 @@
-from Datastore import df
-from Feature_Extraction import vector
-from new_training import vect
-from new_training import dframe
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score
 
+#from Datastore import df
+#from Feature_Extraction import vector
+#from new_training import vect
+#from new_training import dframe
+#from sklearn.model_selection import train_test_split
+#from sklearn.naive_bayes import MultinomialNB
+#from sklearn.metrics import accuracy_score
+
+import nltk
 import matplotlib.pyplot as plt
-print('hi ')
+from sentiment import preprocessed_test_set
+from Feature_Extraction import training_features
+from Feature_Extraction import extract_features
+
+NBayesClassifier = nltk.NaiveBayesClassifier.train(training_features)
+NBResultLabels = [NBayesClassifier.classify(extract_features(tweet[0])) for tweet in preprocessed_test_set]
+print(NBResultLabels)
 
 #for training dataset -->  (0 = negative, 2 = neutral, 4 = positive)
 
-X_train,y_train=vect,dframe.sentiment
-X_test = vector
-y_test = df.sentiment
+#X_train,y_train=vect,dframe.sentiment
+#type of X_train = <class 'numpy.ndarray'>
+#print(f"type of X_train = {type(X_train)}")
+#X_test = vector
+#y_test = df.sentiment
 #X_train,X_test,y_train,y_test=train_test_split(X,y,random_state=30)
-clf = MultinomialNB().fit(X_train, y_train)
-y_pred=clf.predict(X_test)
-print(accuracy_score(y_test,y_pred))
+#clf = MultinomialNB().fit(X_train, y_train)
+#y_pred = clf.predict(X_test)
+#print(accuracy_score(y_test, y_pred))
 
+p,n,nt = 0,0,0
 
-p,n,nt=0,0,0
-for i in y_pred:
-    if i=="Positive":
-        p=p+1;
-    elif i=="Negative":
-        n=n+1;
-    else:
+for i in NBResultLabels:
+    if i=='3':
         nt=nt+1;
-fig = plt.figure()
-ax = fig.add_axes([0,0,1,1])
-Sentiment = ['Positive', 'Neutral', 'Negative']
+    elif i=='1':
+        n=n+1;
+    elif i=='5':
+        p=p+1;
+
+Sentiment_label = ['Positive','Neutral','Negative']
 NoT= [p,nt,n]
-ax.bar(Sentiment,NoT)
-plt.show()
+print(f'positive = {p}')
+print(f'negative = {n}')
+print(f'neutral = {nt}')
+
