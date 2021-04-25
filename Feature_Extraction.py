4d8696fb92923165fc853455110ce555fe4e8df2
courageoustouch <rashiguptafeb@gmail.com>
2021-04-25 10:00:51 +0530

    commiting feature_extraction

--- Feature_Extraction.py
+++ Feature_Extraction.py
@@ -1,11 +1,28 @@
-from sklearn.feature_extraction.text import CountVectorizer 
-from preprocessing import tweets
+from final_training_set import preprocessed_training_set
+import nltk
 
-# Create a vectorizer Object
-vectorizer = CountVectorizer() 
-  
-vectorizer.fit(tweets) 
-vector = vectorizer.transform(tweets).toarray()
+word_features = []
+
+def buildVocabulary(preprocessed_training_set):
+    all_words = []
+    for (ws, senti) in preprocessed_training_set:
+        all_words.extend(ws)
+    wordlist = nltk.FreqDist(all_words)
+    word_features = wordlist.keys()
+    return word_features
+
+def extract_features(take_tweet):
+    tweet_words = set(take_tweet)
+    features = {}
+    for take_word in word_features:
+        features['contains(%s)' % take_word] = (take_word in tweet_words)
+    return features
+
+word_fs = buildVocabulary(preprocessed_training_set)
+training_features = nltk.classify.apply_features(extract_features, preprocessed_training_set)
+
+
+
 
 
 
